3baa8c2701047b768377d69b8d5fdb41c64b7000
Fombi Favour <fombifavour@gmail.com>
2022-03-01 18:13:50 +0100

    Add project files.

---
+++ Library Management System/MenuForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Library_Management_System
+{
+    partial class MenuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuForm));
+            this.panelMenu = new Guna.UI2.WinForms.Guna2Panel();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.btnIssue = new System.Windows.Forms.Button();
+            this.btnBooks = new System.Windows.Forms.Button();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.PictureBox();
+            this.panelTitleBar = new Guna.UI2.WinForms.Guna2Panel();
+            this.lblTitleChildForm = new System.Windows.Forms.Label();
+            this.iconCurrentChildForm = new System.Windows.Forms.PictureBox();
+            this.panelShadow = new Guna.UI2.WinForms.Guna2Panel();
+            this.panelDesktop = new Guna.UI2.WinForms.Guna2Panel();
+            this.panelMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnHome)).BeginInit();
+            this.panelTitleBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconCurrentChildForm)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(35)))), ((int)(((byte)(49)))));
+            this.panelMenu.Controls.Add(this.btnSettings);
+            this.panelMenu.Controls.Add(this.btnLogout);
+            this.panelMenu.Controls.Add(this.btnReturn);
+            this.panelMenu.Controls.Add(this.btnIssue);
+            this.panelMenu.Controls.Add(this.btnBooks);
+            this.panelMenu.Controls.Add(this.btnDashboard);
+            this.panelMenu.Controls.Add(this.btnHome);
+            resources.ApplyResources(this.panelMenu, "panelMenu");
+            this.panelMenu.Name = "panelMenu";
+            // 
+            // btnSettings
+            // 
+            resources.ApplyResources(this.btnSettings, "btnSettings");
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.ForeColor = System.Drawing.Color.Silver;
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+            // 
+            // btnLogout
+            // 
+            resources.ApplyResources(this.btnLogout, "btnLogout");
+            this.btnLogout.FlatAppearance.BorderSize = 0;
+            this.btnLogout.ForeColor = System.Drawing.Color.Silver;
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // btnReturn
+            // 
+            resources.ApplyResources(this.btnReturn, "btnReturn");
+            this.btnReturn.FlatAppearance.BorderSize = 0;
+            this.btnReturn.ForeColor = System.Drawing.Color.Silver;
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
+            // 
+            // btnIssue
+            // 
+            resources.ApplyResources(this.btnIssue, "btnIssue");
+            this.btnIssue.FlatAppearance.BorderSize = 0;
+            this.btnIssue.ForeColor = System.Drawing.Color.Silver;
+            this.btnIssue.Name = "btnIssue";
+            this.btnIssue.UseVisualStyleBackColor = true;
+            this.btnIssue.Click += new System.EventHandler(this.btnIssue_Click);
+            // 
+            // btnBooks
+            // 
+            resources.ApplyResources(this.btnBooks, "btnBooks");
+            this.btnBooks.FlatAppearance.BorderSize = 0;
+            this.btnBooks.ForeColor = System.Drawing.Color.Silver;
+            this.btnBooks.Name = "btnBooks";
+            this.btnBooks.UseVisualStyleBackColor = true;
+            this.btnBooks.Click += new System.EventHandler(this.btnBooks_Click);
+            // 
+            // btnDashboard
+            // 
+            resources.ApplyResources(this.btnDashboard, "btnDashboard");
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.ForeColor = System.Drawing.Color.Silver;
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.UseVisualStyleBackColor = true;
+            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
+            // 
+            // btnHome
+            // 
+            resources.ApplyResources(this.btnHome, "btnHome");
+            this.btnHome.Name = "btnHome";
+            this.btnHome.TabStop = false;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(35)))), ((int)(((byte)(49)))));
+            this.panelTitleBar.Controls.Add(this.lblTitleChildForm);
+            this.panelTitleBar.Controls.Add(this.iconCurrentChildForm);
+            resources.ApplyResources(this.panelTitleBar, "panelTitleBar");
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitleBar_MouseDown);
+            // 
+            // lblTitleChildForm
+            // 
+            resources.ApplyResources(this.lblTitleChildForm, "lblTitleChildForm");
+            this.lblTitleChildForm.ForeColor = System.Drawing.Color.Silver;
+            this.lblTitleChildForm.Name = "lblTitleChildForm";
+            // 
+            // iconCurrentChildForm
+            // 
+            this.iconCurrentChildForm.Image = global::Library_Management_System.Properties.Resources.icons8_home_32;
+            resources.ApplyResources(this.iconCurrentChildForm, "iconCurrentChildForm");
+            this.iconCurrentChildForm.Name = "iconCurrentChildForm";
+            this.iconCurrentChildForm.TabStop = false;
+            // 
+            // panelShadow
+            // 
+            this.panelShadow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(35)))), ((int)(((byte)(49)))));
+            resources.ApplyResources(this.panelShadow, "panelShadow");
+            this.panelShadow.Name = "panelShadow";
+            // 
+            // panelDesktop
+            // 
+            this.panelDesktop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(40)))), ((int)(((byte)(74)))));
+            resources.ApplyResources(this.panelDesktop, "panelDesktop");
+            this.panelDesktop.Name = "panelDesktop";
+            // 
+            // MenuForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panelDesktop);
+            this.Controls.Add(this.panelShadow);
+            this.Controls.Add(this.panelTitleBar);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "MenuForm";
+            this.panelMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnHome)).EndInit();
+            this.panelTitleBar.ResumeLayout(false);
+            this.panelTitleBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconCurrentChildForm)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel panelMenu;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.Button btnIssue;
+        private System.Windows.Forms.Button btnBooks;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.PictureBox btnHome;
+        private Guna.UI2.WinForms.Guna2Panel panelTitleBar;
+        private System.Windows.Forms.PictureBox iconCurrentChildForm;
+        private System.Windows.Forms.Label lblTitleChildForm;
+        private Guna.UI2.WinForms.Guna2Panel panelShadow;
+        private Guna.UI2.WinForms.Guna2Panel panelDesktop;
+    }
+}
