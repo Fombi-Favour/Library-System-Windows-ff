92e7c369cdc6bca8df420d72b21da952f062d250
Fombi Favour <fombifavour@gmail.com>
2022-03-01 23:48:45 +0100

    created language settings and loading forms

---
+++ Library Management System/LanguageForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library_Management_System
+{
+    public partial class LanguageForm : Form
+    {
+        public LanguageForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEn_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.lang = "en-US";
+            new loadingForm().ShowDialog();
+            this.Close();
+        }
+
+        private void btnFr_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.lang = "fr";
+            new loadingForm().ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ Library Management System/loadingForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library_Management_System
+{
+    public partial class loadingForm : Form
+    {
+        public loadingForm()
+        {
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(Properties.Settings.Default.lang);
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ProgressBar.Value += 1;
+            if(ProgressBar.Value == 100)
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void loadingForm_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
